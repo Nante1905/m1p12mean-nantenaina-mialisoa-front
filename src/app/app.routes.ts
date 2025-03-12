b0995118c27baeec76659dbad2d168888269342f
Nante <nantemino15@gmail.com>
2025-03-12 15:39:52 +0300

    feat : [sidebar] styling sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TestComponentComponent } from './test/components/test-component/test-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'test',
+    component: TestComponentComponent,
+  },
+];
