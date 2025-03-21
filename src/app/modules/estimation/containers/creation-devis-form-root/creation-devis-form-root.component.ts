21fdd70d7857d072dee843e703515c38edf8f41d
Mialy05 <mialisoamurielle@gmail.com>
2025-03-21 11:01:17 +0300

    feat [create-devis] pre-fill form for quote creationif it comes from quote request

--- src/app/modules/estimation/containers/creation-devis-form-root/creation-devis-form-root.component.ts
+++ src/app/modules/estimation/containers/creation-devis-form-root/creation-devis-form-root.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 import { catchError, finalize, map, Observable, of } from 'rxjs';
+import { DemandeDevis } from '../../../../shared/types/DemandeDevis';
 import { Marque } from '../../../../shared/types/Marque';
 import { Motorisation } from '../../../../shared/types/Motorisation';
 import { Service } from '../../../../shared/types/Services';
@@ -22,20 +24,56 @@ import { DevisCreationType } from '../../types/DevisCreationType';
 export class CreationDevisFormRootComponent implements OnInit {
   constructor(
     private devisService: DevisService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private route: ActivatedRoute
   ) {}
 
   services$!: Observable<Service[]>;
   marques$!: Observable<Marque[]>;
   motorisations$!: Observable<Motorisation[]>;
+  demandeDevis$!: Observable<DemandeDevis | null>;
   loading = false;
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      console.log(params);
+      const idDemande = params['from'];
+      if (idDemande) {
+        console.log('Valeur de idDemande:', idDemande);
+        this.loading = true;
+        this.demandeDevis$ = this.devisService
+          .findDemandeDevisById(idDemande)
+          .pipe(
+            map((res) => {
+              this.marques$ = of([res.data.vehicule.marque as Marque]);
+              this.motorisations$ = of([
+                res.data.vehicule.motorisation as Motorisation,
+              ]);
+              console.log('tonga demande devis');
+
+              return res.data;
+            }),
+            catchError((err) => {
+              this.messageService.add({
+                severity: 'error',
+                summary: 'Erreur',
+                detail: err.error.message,
+              });
+              return of(null);
+            }),
+            finalize(() => {
+              this.loading = false;
+            })
+          );
+      } else {
+        this.demandeDevis$ = of(null);
+        this.marques$ = this.devisService.findAllMarque();
+        this.motorisations$ = this.devisService.findAllMotorisation();
+      }
+    });
     this.services$ = this.devisService
       .findAllServices()
       .pipe(map((res) => res.data));
-    this.marques$ = this.devisService.findAllMarque();
-    this.motorisations$ = this.devisService.findAllMotorisation();
   }
 
   handleSubmit(data: DevisCreationType) {
