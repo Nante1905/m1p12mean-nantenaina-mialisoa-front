15b1faa4708d57bb6cf0c434ae77e39b37c84581
Mialy05 <mialisoamurielle@gmail.com>
2025-04-04 00:15:06 +0300

    feat [dashboard] add date filter to dashboard

--- src/app/modules/dashboard/containers/dashboard-mec-root/dashboard-mec-root.component.ts
+++ src/app/modules/dashboard/containers/dashboard-mec-root/dashboard-mec-root.component.ts
@@ -1,26 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { SkeletonModule } from 'primeng/skeleton';
 import { TableModule } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
 import { map } from 'rxjs';
 import { CardNumberComponent } from '../../components/card-number/card-number.component';
+import { PeriodeFilter } from '../../constants/dashboard';
 import { DashboardService } from '../../services/dashboard.service';
 import { TaskResumeDTO } from '../../types/dashboard.type';
 
 @Component({
   selector: 'app-dashboard-mec-root',
-  imports: [CardNumberComponent, TableModule, TagModule],
+  imports: [CardNumberComponent, TableModule, TagModule, SkeletonModule],
   templateUrl: './dashboard-mec-root.component.html',
   styleUrl: './dashboard-mec-root.component.scss',
 })
-export class DashboardMecRootComponent implements OnInit {
+export class DashboardMecRootComponent implements OnInit, OnChanges {
   constructor(private dashboardService: DashboardService) {}
 
   nbrInterventionMec: number = 0;
   workedHoursMec: number = 0;
   taskResume: TaskResumeDTO[] = [];
 
+  loadingNbrInterventionMec: boolean = true;
+  loadingWorkedHoursMec: boolean = true;
+  loadingTaskResume: boolean = true;
+
+  filterDate = input<PeriodeFilter>();
+
   ngOnInit(): void {
     this.initDashboardMec();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const filter = changes['filterDate'].currentValue;
+    if (filter) {
+      this.initDashboardMec(filter);
+    }
   }
 
   getClassName = (status: string) => {
@@ -33,25 +54,32 @@ export class DashboardMecRootComponent implements OnInit {
       .replace(/[\u0300-\u036f]/g, '')}`;
   };
 
-  initDashboardMec() {
+  initDashboardMec(range?: PeriodeFilter) {
+    this.loadingNbrInterventionMec = true;
+    this.loadingWorkedHoursMec = true;
+    this.loadingTaskResume = true;
+
     this.dashboardService
-      .getNbrInterventionOfMec()
+      .getNbrInterventionOfMec(range)
       .pipe(map((res) => res.data))
       .subscribe((res) => {
         this.nbrInterventionMec = res.interventions;
+        this.loadingNbrInterventionMec = false;
       });
 
     this.dashboardService
-      .getWorkedHoursOfMec()
+      .getWorkedHoursOfMec(range)
       .pipe(map((res) => res.data))
       .subscribe((res) => {
         this.workedHoursMec = res.heures;
+        this.loadingWorkedHoursMec = false;
       });
     this.dashboardService
-      .getTaskResume()
+      .getTaskResume(range)
       .pipe(map((res) => res.data))
       .subscribe((res) => {
         this.taskResume = res;
+        this.loadingTaskResume = false;
       });
   }
 }
